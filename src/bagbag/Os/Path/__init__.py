0379b6a4ab24a68f9e60e771d8456de0bf8b37b2
localbuild <localbuild@gitea.com>
2022-07-31 22:32:21 +0400

    0.20.4

--- src/bagbag/Os/Path/__init__.py
+++ src/bagbag/Os/Path/__init__.py
@@ -2,3 +2,9 @@ import os
 
 def Basedir(path:str) -> str:
     return os.path.dirname(path)
+
+def Join(*path) -> str:
+    return os.path.join(*path)
+
+if __name__ == "__main__":
+    print(Join("a", "b"))
